0dad6f7c28a1073e9c08195f4d86d1330047896d
Thai Duong <duongphuthai@gmail.com>
2019-05-29 16:26:16 -0700

    add option to pick goal from RViz

--- include/car_control/power_diagram_controller.hpp
+++ include/car_control/power_diagram_controller.hpp
@@ -11,68 +11,75 @@ namespace car_control {
  * Class containing the Power Diagram Controller
  */
 class PowerDiagramController {
-public:
-	/*!
-	 * Constructor.
-	 */
-  PowerDiagramController(ros::NodeHandle& nodeHandle);
+ public:
+    /*!
+     * Constructor.
+     */
+    PowerDiagramController(ros::NodeHandle& nodeHandle);
 
-	/*!
-	 * Destructor.
-	 */
-	virtual ~PowerDiagramController();
-  /*!
-   * Get params from launch file.
-   */
-	bool getParams();
-  /*!
-   * Odometry callback
-   */
-	void odomCallback(const nav_msgs::Odometry& odoms);
-  /*!
-   * calculate the position on the path at normalized time t, 0<=t<=1.
-   */
-	void calculatePath(double t, double x0, double y0, double &xt, double &yt);
-  /*!
-   * Generate path
-   */
-	void genPath(int point_count);
-  /*!
-   * Publish path
-   */
-	void pubPath(const nav_msgs::Odometry& odoms);
-  /*!
-   * Calculate twist according to power diagram controller
-   */
-	geometry_msgs::Twist getCmd();
+    /*!
+     * Destructor.
+     */
+    virtual ~PowerDiagramController();
+    /*!
+     * Get params from launch file.
+     */
+    bool getParams();
+    /*!
+     * Odometry callback
+     */
+    void odomCallback(const nav_msgs::Odometry& odoms);
+    /*!
+     * Odometry callback
+     */
+    void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+    /*!
+     * calculate the position on the path at normalized time t, 0<=t<=1.
+     */
+    void calculatePath(double t, double x0, double y0, double &xt, double &yt);
+    /*!
+     * Generate path
+     */
+    void genPath(int point_count);
+    /*!
+     * Publish path
+     */
+    void pubPath(const nav_msgs::Odometry& odoms);
+    /*!
+     * Calculate twist according to power diagram controller
+     */
+    geometry_msgs::Twist getCmd();
 
-private:
-	// Publishers and Subscribers
-	ros::NodeHandle node_handle;
-	ros::Publisher cmd_pub;
-	ros::Publisher path_pub;
-	ros::Subscriber odom_sub;
-	std::string cmd_topic, path_topic, odom_topic;
+ private:
+    // Publishers and Subscribers
+    ros::NodeHandle node_handle;
+    ros::Publisher cmd_pub;
+    ros::Publisher path_pub;
+    ros::Subscriber odom_sub;
+    ros::Subscriber goal_sub;
+    std::string cmd_topic, path_topic, odom_topic;
 
-	// Current robot pose
-	double odom_x;
-	double odom_y;
-	double odom_yaw;
-	// Initial robot's position (start) and time.
-	double init_x;
-	double init_y;
-	ros::Time init_t;
+    // Current robot pose
+    double odom_x;
+    double odom_y;
+    double odom_yaw;
+    // Initial robot's position (start) and time.
+    double init_x;
+    double init_y;
+    ros::Time init_t;
 
-	// Path params
-	bool update_path = true;
-	nav_msgs::Path path;
+    // Path params
+    bool update_path = true;
+    nav_msgs::Path path;
+    geometry_msgs::PoseStamped::ConstPtr goal;
+    bool is_user_defined_goal = false;
 
-  // Controller params
-  // T is the desired time to go from start to goal
-  double T;
-	// Controller gains
-	double kl, ka, goal_threshold;
-	double vx, vy, ax, ay;
+    // Controller params
+    // T is the desired time to go from start to goal
+    double T;
+    // Controller gains
+    double kl, ka, goal_threshold;
+    double vx, vy, ax, ay;
 
 };
 
--- src/power_diagram_controller.cpp
+++ src/power_diagram_controller.cpp
@@ -10,18 +10,19 @@
 namespace car_control {
 
 PowerDiagramController::PowerDiagramController(ros::NodeHandle& nodeHandle) :
-    node_handle(nodeHandle)
+            node_handle(nodeHandle)
 {
-  if (!this->getParams())
-  {
-    ROS_ERROR("Missing parameters");
-    ros::requestShutdown();
-  }
-
-  path = nav_msgs::Path();
-  cmd_pub = node_handle.advertise<geometry_msgs::Twist>( cmd_topic, 1 );
-  path_pub= node_handle.advertise<nav_msgs::Path>( path_topic, 1 );
-  odom_sub= node_handle.subscribe(odom_topic,1,&PowerDiagramController::odomCallback,this);
+    if (!this->getParams())
+    {
+        ROS_ERROR("Missing parameters");
+        ros::requestShutdown();
+    }
+    goal = NULL;
+    path = nav_msgs::Path();
+    cmd_pub = node_handle.advertise<geometry_msgs::Twist>( cmd_topic, 1 );
+    path_pub= node_handle.advertise<nav_msgs::Path>( path_topic, 1 );
+    odom_sub= node_handle.subscribe(odom_topic,1,&PowerDiagramController::odomCallback,this);
+    goal_sub = node_handle.subscribe("/move_base_simple/goal", 1, &PowerDiagramController::goalCallback,this);
 }
 
 PowerDiagramController::~PowerDiagramController()
@@ -30,97 +31,120 @@ PowerDiagramController::~PowerDiagramController()
 
 bool PowerDiagramController::getParams()
 {
-  if (!node_handle.getParam("/power_diagram_controller/cmd_vel",cmd_topic))
-    return false;
-  ROS_INFO("cmd_topic = %s", cmd_topic.c_str());
-  if (!node_handle.getParam("/power_diagram_controller/path", path_topic))
-    return false;
-  ROS_INFO("path_topic = %s", path_topic.c_str());
-  if (!node_handle.getParam("/power_diagram_controller/odom", odom_topic))
-    return false;
-  ROS_INFO("odom_topic = %s", odom_topic.c_str());
-  if (!node_handle.getParam("/power_diagram_controller/kl", kl))
-    return false;
-  ROS_INFO("kl = %f", kl);
-  if (!node_handle.getParam("/power_diagram_controller/ka", ka))
-    return false;
-  ROS_INFO("ka = %f", ka);
-  if (!node_handle.getParam("/power_diagram_controller/goal_threshold", goal_threshold))
-    goal_threshold = 0.1;
-  if (!node_handle.getParam("/power_diagram_controller/T", T))
-    T = 10;
-  if (!node_handle.getParam("/power_diagram_controller/vx", vx))
-      vx = 1.0;
-  if (!node_handle.getParam("/power_diagram_controller/vy", vy))
-      vy = 0.0;
-  if (!node_handle.getParam("/power_diagram_controller/ax", ax))
-      ax = 0.0;
-  if (!node_handle.getParam("/power_diagram_controller/ay", ay))
-      ay = 0.0;
-  return true;
+    if (!node_handle.getParam("/power_diagram_controller/cmd_vel",cmd_topic))
+        return false;
+    ROS_INFO("cmd_topic = %s", cmd_topic.c_str());
+    if (!node_handle.getParam("/power_diagram_controller/path", path_topic))
+        return false;
+    ROS_INFO("path_topic = %s", path_topic.c_str());
+    if (!node_handle.getParam("/power_diagram_controller/odom", odom_topic))
+        return false;
+    ROS_INFO("odom_topic = %s", odom_topic.c_str());
+    if (!node_handle.getParam("/power_diagram_controller/kl", kl))
+        return false;
+    ROS_INFO("kl = %f", kl);
+    if (!node_handle.getParam("/power_diagram_controller/ka", ka))
+        return false;
+    ROS_INFO("ka = %f", ka);
+    if (!node_handle.getParam("/power_diagram_controller/goal_threshold", goal_threshold))
+        goal_threshold = 0.1;
+    if (!node_handle.getParam("/power_diagram_controller/T", T))
+        T = 10;
+    if (!node_handle.getParam("/power_diagram_controller/vx", vx))
+        vx = 1.0;
+    if (!node_handle.getParam("/power_diagram_controller/vy", vy))
+        vy = 0.0;
+    if (!node_handle.getParam("/power_diagram_controller/ax", ax))
+        ax = 0.0;
+    if (!node_handle.getParam("/power_diagram_controller/ay", ay))
+        ay = 0.0;
+    if (!node_handle.getParam("/power_diagram_controller/user_defined_goal", is_user_defined_goal))
+        is_user_defined_goal = false;
+    return true;
 }
 void PowerDiagramController::odomCallback(const nav_msgs::Odometry& odoms)
 {
-  // Publish path
-  PowerDiagramController::pubPath(odoms);
+    // Publish path
+    PowerDiagramController::pubPath(odoms);
 
-  // Calculate x, y, and yaw from odom
-  odom_x = odoms.pose.pose.position.x;
-  odom_y = odoms.pose.pose.position.y;
-  odom_yaw = tf::getYaw(odoms.pose.pose.orientation);
-  //ROS_INFO("The car location is [%.3f,%.3f],%.3f",odom_x,odom_y,odom_yaw);
+    // Calculate x, y, and yaw from odom
+    odom_x = odoms.pose.pose.position.x;
+    odom_y = odoms.pose.pose.position.y;
+    odom_yaw = tf::getYaw(odoms.pose.pose.orientation);
+    //ROS_INFO("The car location is [%.3f,%.3f],%.3f",odom_x,odom_y,odom_yaw);
 
-  // Get Twist commands
-  geometry_msgs::Twist cmd_vel = this->getCmd();
+    // Get Twist commands
+    geometry_msgs::Twist cmd_vel = this->getCmd();
 
-  // Publish cmd_vel
-  cmd_pub.publish(cmd_vel);
+    // Publish cmd_vel
+    cmd_pub.publish(cmd_vel);
+}
+
+void PowerDiagramController::goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    goal = msg;
 }
 
 geometry_msgs::Twist PowerDiagramController::getCmd()
 {
-  ros::Time current_t = ros::Time::now();
-  ros::Duration delta_t = current_t - init_t;
-  // Normalized time
-  double t = std::min(delta_t.toSec(), T)/T;
-  double goal_x, goal_y;
-  // Current goal to follow at time t
-  this->calculatePath(t, init_x, init_y, goal_x, goal_y);
-  double linear_vel;
-  double angular_vel;
+    ros::Time current_t = ros::Time::now();
+    ros::Duration delta_t = current_t - init_t;
+    // Normalized time
+    double t = std::min(delta_t.toSec(), T)/T;
+    double goal_x, goal_y;
+    if (is_user_defined_goal)
+    {
+        if (goal)
+        {
+            goal_x = goal->pose.position.x;
+            goal_y = goal->pose.position.y;
+        }
+        else
+        {
+            goal_x = odom_x;
+            goal_y = odom_y;
+        }
+    }
+    else
+    {
+        // Current goal to follow at time t
+            this->calculatePath(t, init_x, init_y, goal_x, goal_y);
+    }
+    double linear_vel;
+    double angular_vel;
 
-  if ((fabs(odom_x - goal_x) < goal_threshold) && (fabs(odom_y - goal_y) < goal_threshold))
-  {
-    // Reached goal! Stop here!
-    linear_vel = 0;
-    angular_vel = 0;
-  }
-  else
-  {
-    // power diagram control
-    linear_vel = -kl*(cos(odom_yaw)*(odom_x - goal_x) + sin(odom_yaw)*(odom_y - goal_y));
-    angular_vel = -ka*atan2(-sin(odom_yaw)*(odom_x - goal_x) + cos(odom_yaw)*(odom_y - goal_y), cos(odom_yaw)*(odom_x - goal_x) + sin(odom_yaw)*(odom_y - goal_y));
-  }
+    if ((fabs(odom_x - goal_x) < goal_threshold) && (fabs(odom_y - goal_y) < goal_threshold))
+    {
+        // Reached goal! Stop here!
+        linear_vel = 0;
+        angular_vel = 0;
+    }
+    else
+    {
+        // power diagram control
+        linear_vel = -kl*(cos(odom_yaw)*(odom_x - goal_x) + sin(odom_yaw)*(odom_y - goal_y));
+        angular_vel = -ka*atan2(-sin(odom_yaw)*(odom_x - goal_x) + cos(odom_yaw)*(odom_y - goal_y), cos(odom_yaw)*(odom_x - goal_x) + sin(odom_yaw)*(odom_y - goal_y));
+    }
 
-  geometry_msgs::Twist cmd_vel = geometry_msgs::Twist();
-  cmd_vel.linear.x = linear_vel;
-  cmd_vel.angular.z = angular_vel;
-  return cmd_vel;
+    geometry_msgs::Twist cmd_vel = geometry_msgs::Twist();
+    cmd_vel.linear.x = linear_vel;
+    cmd_vel.angular.z = angular_vel;
+    return cmd_vel;
 }
 
 void PowerDiagramController::pubPath(const nav_msgs::Odometry& odoms)
 {
-  // Only recalculate the path if we want to update. Otherwise, publish the current path.
+    // Only recalculate the path if we want to update. Otherwise, publish the current path.
     if (update_path)
     {
-      update_path = false;
-      init_x = odoms.pose.pose.position.x;
-      init_y = odoms.pose.pose.position.y;
-      init_t = ros::Time::now();
-      path = nav_msgs::Path();
-      path.header.stamp = init_t;
-      path.header.frame_id = odoms.header.frame_id;
-      this->genPath(1000);
+        update_path = false;
+        init_x = odoms.pose.pose.position.x;
+        init_y = odoms.pose.pose.position.y;
+        init_t = ros::Time::now();
+        path = nav_msgs::Path();
+        path.header.stamp = init_t;
+        path.header.frame_id = odoms.header.frame_id;
+        this->genPath(1000);
     }
 
     // Publish path
@@ -129,25 +153,25 @@ void PowerDiagramController::pubPath(const nav_msgs::Odometry& odoms)
 
 void PowerDiagramController::genPath(int point_count)
 {
-  double xt;
-  double yt;
-  for(int i = 0; i<= point_count; i++)
-  {
-    geometry_msgs::PoseStamped pose = geometry_msgs::PoseStamped();
-    pose.header.frame_id = path.header.frame_id;
-    pose.header.stamp = ros::Time::now();
-    double t = double(i)/point_count;
-    this->calculatePath(t, init_x, init_y, xt, yt);
-    pose.pose.position.x = xt;
-    pose.pose.position.y = yt;
-    path.poses.push_back(pose);
-  }
+    double xt;
+    double yt;
+    for(int i = 0; i<= point_count; i++)
+    {
+        geometry_msgs::PoseStamped pose = geometry_msgs::PoseStamped();
+        pose.header.frame_id = path.header.frame_id;
+        pose.header.stamp = ros::Time::now();
+        double t = double(i)/point_count;
+        this->calculatePath(t, init_x, init_y, xt, yt);
+        pose.pose.position.x = xt;
+        pose.pose.position.y = yt;
+        path.poses.push_back(pose);
+    }
 }
 
 void PowerDiagramController::calculatePath(double t, double x0, double y0, double &xt, double &yt)
 {
-  xt = x0 + vx*t + ax*t*t/2;
-  yt = y0 + vy*t + ay*t*t/2;
+    xt = x0 + vx*t + ax*t*t/2;
+    yt = y0 + vy*t + ay*t*t/2;
 }
 
 } /* namespace */
